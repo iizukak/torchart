c2c780b3103b0281a536a8155894ae77d1b47fe2
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2020-12-26 23:58:39 +0900

    Add isort (#7)

--- torchart/filtervisualization/test_filter_visualizer.py
+++ torchart/filtervisualization/test_filter_visualizer.py
@@ -5,18 +5,18 @@ import torch
 from torchart.filtervisualization.filter_visualizer import FilterVisualizer
 
 
-def test_init_filter_visualizer():
+def test_init_filter_visualizer() -> None:
     visualizer = FilterVisualizer(100, 3, 1, 1.5, 0.1, 1, "cpu")
     assert len(visualizer.model.features) == 31
 
 
-def test_up_scaling():
+def test_up_scaling() -> None:
     visualizer = FilterVisualizer(100, 3, 1, 1.5, 0.1, 1, "cpu")
     visualizer.train(0)
     assert visualizer.output.shape == torch.Size([1, 3, 150, 150])
 
 
-def test_save_output_image():
+def test_save_output_image() -> None:
     test_file_path = "./vgg16_3_0.png"
     if os.path.exists(test_file_path):
         os.remove(test_file_path)
